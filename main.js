4a91c06d008f9411c2d81b985ce5db0cd91c6d54
walter-taramasco <77883391+WaltApps@users.noreply.github.com>
2021-02-06 10:39:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.conejos', { delay: 500 });
+ScrollReveal().reveal('.conejos-banner-one', { delay: 500 });
+ScrollReveal().reveal('.accesorios-banner-two', { delay: 500 });
+ScrollReveal().reveal('.acerca-banner-three', { delay: 500 });
